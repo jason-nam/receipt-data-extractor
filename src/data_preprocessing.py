1b04f5f6896a167d67f7306c60fd3ce3ec651745
jason_yunjae_nam <jasonnam0416@DESKTOP-JDD2PFO.localdomain>
2023-10-22 23:12:06 -0400

    added more preprocessing methods

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,5 +1,5 @@
-import cv2
 import numpy as np
+import cv2
 from pathlib import Path
 
 IN_PATH = '../data/unprocessed_images/'
@@ -15,22 +15,42 @@ def remove_noise(image):
  
 # thresholding
 def thresholding(image):
-    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    return cv2.threshold(image, 0, 255, 
+                         cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+# adaptive thresholding
+def adaptive_thresholding(image):
+    return cv2.adaptiveThreshold(image, 255,
+                                 cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                 cv2.THRESH_BINARY_INV, 11, 2)
 
 # dilation
 def dilate(image):
     kernel = np.ones((5,5),np.uint8)
-    return cv2.dilate(image, kernel, iterations = 1)
+    return cv2.dilate(image, kernel, iterations=1)
+
+# gaussian blur
+def gaussian_blur(image):
+    return cv2.GaussianBlur(image, (15, 15), 0)
+
+# flatten
+def flatten(cleaned, background):
+    return 255 - cv2.divide(cleaned, 255 - background, scale=256)
+
+# increase contrast
+def increase_contrast(image):
+    return cv2.convertScaleAbs(image, alpha=1.5, beta=-50) # alpha (contrast) and beta (brightness)
     
 # erosion
 def erode(image):
     kernel = np.ones((5,5),np.uint8)
     return cv2.erode(image, kernel, iterations = 1)
 
-# opening - erosion followed by dilation
+# reducing noise - erosion followed by dilation
 def opening(image):
     kernel = np.ones((5,5),np.uint8)
-    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    cleaned = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    return cv2.morphologyEx(cleaned, cv2.MORPH_CLOSE, kernel)
 
 # canny edge detection
 def canny(image):
@@ -53,6 +73,33 @@ def deskew(image):
 # template matching
 def match_template(image, template):
     return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
+
+if __name__ == '__main__':
+    # list all image files in the input directory
+    in_files = list(Path(IN_PATH).glob('*.[jJ][pP]*[gG]'))
+    
+    for in_file in in_files:
+        # read the image
+        image = cv2.imread(str(in_file))
+        
+        # apply preprocessing steps
+        image = get_grayscale(image)
+        image = remove_noise(image)
+        image = gaussian_blur(image)
+        image = adaptive_thresholding(image)
+        image = deskew(image)
+        image = opening(image) # idk
+        
+        # dilate can help you get a rough estimate of the background.
+        background = dilate(image)
+        image = flatten(image, background)
+        
+        image = increase_contrast(image) # might not be necessary
+        
+        # save the preprocessed image to the output directory
+        out_file = Path(OUT_PATH) / in_file.name
+        cv2.imwrite(str(out_file), image)
+
 
 if __name__ == '__main__':
 
