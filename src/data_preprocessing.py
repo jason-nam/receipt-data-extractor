e81646251161c6493fa49a5312efc745d0f49eaa
namy2 <namy2@mcmaster.ca>
2023-10-30 14:49:07 -0400

    updated text detection and recognition; text detection detects every box; data preprocessing to be further optimized

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -10,7 +10,7 @@ def get_grayscale(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 # noise removal
-def remove_noise(image):
+def median_blur(image):
     return cv2.medianBlur(image,5)
  
 # thresholding
@@ -33,10 +33,6 @@ def dilate(image):
 def gaussian_blur(image):
     return cv2.GaussianBlur(image, (15, 15), 0)
 
-# flatten
-def flatten(cleaned, background):
-    return 255 - cv2.divide(cleaned, 255 - background, scale=256)
-
 # increase contrast
 def increase_contrast(image):
     return cv2.convertScaleAbs(image, alpha=1.5, beta=-50) # alpha (contrast) and beta (brightness)
@@ -47,7 +43,7 @@ def erode(image):
     return cv2.erode(image, kernel, iterations = 1)
 
 # reducing noise - erosion followed by dilation
-def opening(image):
+def morphology(image):
     kernel = np.ones((5,5),np.uint8)
     cleaned = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
     return cv2.morphologyEx(cleaned, cv2.MORPH_CLOSE, kernel)
@@ -56,23 +52,38 @@ def opening(image):
 def canny(image):
     return cv2.Canny(image, 100, 200)
 
-# skew correction
 def deskew(image):
     coords = np.column_stack(np.where(image > 0))
     angle = cv2.minAreaRect(coords)[-1]
-    if angle < -45:
-        angle = -(90 + angle)
-    else:
+    
+    # Adjust the angle based on its value
+    if 45 > angle >= 0:
         angle = -angle
+    elif -45 <= angle < 0:
+        angle = -angle
+    elif angle >= 45:
+        angle = 90 - angle
+    
     (h, w) = image.shape[:2]
     center = (w // 2, h // 2)
     M = cv2.getRotationMatrix2D(center, angle, 1.0)
     rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    
     return rotated
+
 
 # template matching
 def match_template(image, template):
     return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED) 
+
+def flatten(cleaned, background):
+    # Calculate the mean value of the background
+    mean_value = np.mean(background)
+    
+    # Subtract the mean value from the cleaned image and normalize to [0, 255]
+    flattened = np.clip(cleaned - mean_value, 0, 255).astype(np.uint8)
+    
+    return flattened
 
 if __name__ == '__main__':
     # list all image files in the input directory
@@ -84,46 +95,52 @@ if __name__ == '__main__':
         
         # apply preprocessing steps
         image = get_grayscale(image)
-        image = remove_noise(image)
-        image = gaussian_blur(image)
-        image = adaptive_thresholding(image)
+        # cv2.imshow("grayscale", image)
+        # cv2.waitKey(0)
+
         image = deskew(image)
-        image = opening(image) # idk
+        # cv2.imshow("deskew", image)
+        # cv2.waitKey(0)
+
+        # image = median_blur(image)
+        # cv2.imshow("remove noise", image)
+        # cv2.waitKey(0)
+
+        image = gaussian_blur(image)
+        # cv2.imshow("gaussian blur", image)
+        # cv2.waitKey(0)
+
+        image = adaptive_thresholding(image)
+        # cv2.imshow("adaptive thresholding", image)
+        # cv2.waitKey(0)
+
+        # image = thresholding(image)
+        # cv2.imshow("thresholding", image)
+        # cv2.waitKey(0)
+
+        # image = erode(image)
+        # cv2.imshow("erosion", image)
+        # cv2.waitKey(0)
+        
+        # image = morphology(image) # idk
+        # cv2.imshow("morphology", image)
+        # cv2.waitKey(0)
         
         # dilate can help you get a rough estimate of the background.
-        background = dilate(image)
-        image = flatten(image, background)
+        # background = dilate(image)
+        # cv2.imshow("dilate", background)
+        # cv2.waitKey(0)
+
+        # image = flatten(image, background)
+        # cv2.imshow("flatten", image)
+        # cv2.waitKey(0)
         
-        image = increase_contrast(image) # might not be necessary
+        # image = increase_contrast(image) # might not be necessary
+        # cv2.imshow("increase contrast", image)
+        # cv2.waitKey(0)
+
+        # cv2.destroyAllWindows()
         
         # save the preprocessed image to the output directory
         out_file = Path(OUT_PATH) / in_file.name
         cv2.imwrite(str(out_file), image)
-
-
-if __name__ == '__main__':
-
-    image_name = 'img2'
-
-    # test
-    image = cv2.imread(IN_PATH + image_name + '.jpg')
-
-    gray = get_grayscale(image)
-    thresh = thresholding(gray)
-    opening = opening(gray)
-    canny = canny(gray)
-
-    # Display images
-    # cv2.imshow("Original Image", image)
-    # cv2.imshow("Grayscale Image", gray)
-    # cv2.imshow("Thresholded Image", thresh)
-    # cv2.imshow("Opening Image", opening)
-    # cv2.imshow("Canny Image", canny)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-    # Save images
-    # cv2.imwrite(processed_image_path + image_name + '_gray_image.png', gray)
-    cv2.imwrite(OUT_PATH + image_name + '.png', thresh)
-    # cv2.imwrite(processed_image_path + image_name + '_opening_image.png', opening)
-    # cv2.imwrite(processed_image_path + image_name + '_canny_image.png', canny)
--- src/text_recognition.py
+++ src/text_recognition.py
@@ -8,14 +8,10 @@ from pathlib import Path
 IN_PATH = '../data/processed_images/'
 OUT_PATH = '../data/image_to_data/'
 
-def extract_image_data(image_name):
+def extract_image_data(image, csv_file_path):
 
-    img = cv2.imread(IN_PATH + image_name + '.png')
-    d = pytesseract.image_to_data(img, output_type=Output.DICT)
+    d = pytesseract.image_to_data(image, output_type=Output.DICT)
     keys = list(d.keys())
-
-    # path to the CSV file
-    csv_file_path = Path(OUT_PATH + image_name + '.csv')
 
     # Writing the OCR results to a CSV file
     with open(csv_file_path, 'w', newline='', encoding='utf-8') as file:
@@ -25,6 +21,16 @@ def extract_image_data(image_name):
             writer.writerow(csv_row)
 
 if __name__ == '__main__':
-    image_name = 'img2'
-    image_type = '_thresholded_image'
-    extract_image_data(image_name)
+
+    in_files = list(Path(IN_PATH).glob('*.[jJ][pP]*[gG]'))
+    
+    for in_file in in_files:
+        
+        image = cv2.imread(str(in_file))
+
+        # path to the CSV file
+        csv_file_path = Path(OUT_PATH + str(in_file.stem) + '.csv')
+
+        extract_image_data(image, csv_file_path)
+
+        print(str(in_file.name))
